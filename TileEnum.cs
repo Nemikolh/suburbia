dc7b888fdc75d5265e8bea963cec20a9a5bb5613
pmourlanne <pmourlanne@gmail.com>
2013-12-19 11:11:02 +0100

    Added generated TileEnum file

---
+++ TileEnum.cs
@@ -0,0 +1,59 @@
+
+using System;
+namespace AssemblyCsharp
+{
+
+public enum ETileIcon
+{
+AIRPORT,
+OFFICE,
+RESTAURANT,
+SCHOOL,
+NEW CAR DEALERSHIP,
+SKYSCRAPER,
+NONE
+}
+
+public enum ETileColor
+{
+BLUE,
+GREY,
+YELLOW,
+GREEN,
+LAKE
+}
+
+public enum ETileLetter
+{
+A,
+B,
+C,
+BASE
+}
+
+public enum ETileScope
+{
+ADJACENT,
+GLOBAL,
+OWN,
+OTHER,
+NONE
+}
+
+public enum ETileResource
+{
+INCOME,
+REPUTATION,
+POPULATION,
+MONEY,
+NONE
+}
+
+public enum ETileWhen
+{
+ALWAYS,
+AFTER,
+AFTER RED LINE
+}
+
+}
