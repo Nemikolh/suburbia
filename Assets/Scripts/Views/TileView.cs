4f02401ef326c7d5418b3e865c3d732260841b67
Varvenne Joan <joan.varvenne@gmail.com>
2014-01-15 15:07:30 +0100

    Small correction for TileView.

--- Assets/Scripts/Views/TileView.cs
+++ Assets/Scripts/Views/TileView.cs
@@ -10,47 +10,51 @@ using System.Collections.Generic;
 
 public class TileView : MonoBehaviour
 {
-    private static UnityEngine.Object m_tile_prefab = Resources.Load("Prefabs/Tile");
-    private static Dictionary<ETileColor, Texture> m_textures = new Dictionary<ETileColor, Texture>();
-
+    private static UnityEngine.Object m_tile_prefab = Resources.Load ("Prefabs/Tile");
+    private static Dictionary<ETileColor, Texture> m_textures = new Dictionary<ETileColor, Texture> ();
     private TileInstance m_tile;
 
-    private TileView()
+    private TileView ()
     {
     }
 
-    public static void InitTextures()
+    public static void InitTextures ()
     {
-        m_textures.Add(ETileColor.BLUE, Resources.Load("Textures/tile_top_blue.png") as Texture);
-        m_textures.Add(ETileColor.YELLOW, Resources.Load("Textures/tile_top_yellow.png") as Texture);
-        m_textures.Add(ETileColor.GREEN, Resources.Load("Textures/tile_top_green.png") as Texture);
-        m_textures.Add(ETileColor.GREY, Resources.Load("Textures/tile_top_blue.png") as Texture);
-        m_textures.Add(ETileColor.LAKE, Resources.Load("Textures/tile_top_blue.png") as Texture);
-        m_textures.Add(ETileColor.NULL, null);
+        m_textures.Add (ETileColor.BLUE, Resources.Load ("Textures/tile_top_blue.png") as Texture);
+        m_textures.Add (ETileColor.YELLOW, Resources.Load ("Textures/tile_top_yellow.png") as Texture);
+        m_textures.Add (ETileColor.GREEN, Resources.Load ("Textures/tile_top_green.png") as Texture);
+        m_textures.Add (ETileColor.GREY, Resources.Load ("Textures/tile_top_blue.png") as Texture);
+        m_textures.Add (ETileColor.LAKE, Resources.Load ("Textures/tile_top_blue.png") as Texture);
+        m_textures.Add (ETileColor.NULL, null);
     }
 
-    public static TileView Instantiate(TileInstance p_instance, Vector3 p_position)
+    public static TileView Instantiate (TileInstance p_instance, Vector3 p_position)
     {
-        // Creation of the new instance.
-        GameObject _new_instance = UnityEngine.Object.Instantiate(m_tile_prefab) as GameObject;
+        try {
+            // Creation of the new instance.
+            GameObject _new_instance = UnityEngine.Object.Instantiate (m_tile_prefab) as GameObject;
 
-        // Set tthe appropriate texture.
-        _new_instance.renderer.material.SetTexture("_PathTex", m_textures[p_instance.color]);
-        _new_instance.renderer.material.SetTexture("_PathMask", m_textures[p_instance.color]);
+            // Set tthe appropriate texture.
+            _new_instance.renderer.material.SetTexture ("_PathTex", m_textures [p_instance.color]);
+            _new_instance.renderer.material.SetTexture ("_PathMask", m_textures [p_instance.color]);
 
-        // Get the script associated with the new tile.
-        TileView _this = _new_instance.GetComponent<TileView>();
-        _this.m_tile = p_instance;
-        _new_instance.transform.position = p_position;
+            // Get the script associated with the new tile.
+            TileView _this = _new_instance.GetComponent<TileView> ();
+            _this.m_tile = p_instance;
+            _new_instance.transform.position = p_position;
 
-        return _this;
+            return _this;
+        } catch (KeyNotFoundException e) {
+            Debug.LogError (e.Data);
+            return null;
+        }
     }
 
-    void Start()
+    void Start ()
     {
     }
 
-    void Update()
+    void Update ()
     {
     }
 }
