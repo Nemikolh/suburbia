489861507f6b03ba6beb8f0ed39acb287b693c5c
pmourlanne <pmourlanne@gmail.com>
2014-01-16 16:14:33 +0100

    Added TestEffect

---
+++ Assets/.test/TestEffect.cs
@@ -0,0 +1,42 @@
+using System;
+
+using NUnit.Framework;
+
+[TestFixture]
+public class TestEffect
+{
+    private Effect effect_money;
+    private Effect effect_reputation;
+
+    [SetUp]
+    public void Init()
+    {
+        effect_reputation = new Effect(ETileResource.REPUTATION, 2);
+        effect_money = new Effect(ETileResource.MONEY, 10);
+    }
+
+    [Test]
+    public void TestApply ()
+    {
+        Player player = new Player();
+
+        Assert.AreEqual(1, player.reputation);
+        Assert.AreEqual(15, player.money);
+
+        effect_money.Apply(player);
+        Assert.AreEqual(25, player.money);
+
+        effect_reputation.Apply(player);
+        Assert.AreEqual(3, player.reputation);
+    }
+
+    [Test]
+    public void TestEquals ()
+    {
+        Effect effect_money_idem = new Effect(ETileResource.MONEY, 10);
+
+        Assert.AreEqual(false, effect_money.Equals(effect_reputation));
+        Assert.AreEqual(true, effect_money.Equals(effect_money_idem));
+    }
+}
+
