8eda6ac93c24a1e6a19e405032fa29f6c534a9fe
Varvenne Joan <joan.varvenne@gmail.com>
2014-01-17 11:22:56 +0100

    Base behavior for the main menu.

---
+++ Assets/MainMenu.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenu : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start ()
+    {
+    }
+    
+    // Update is called once per frame
+    void Update ()
+    {
+    }
+
+    void OnGUI ()
+    {
+        if (GUI.Button (new Rect (50, 50, 200, 60), "Start Two Player Game")) {
+            Suburbia.App.StartGame (2);
+            Instantiate (Resources.Load ("Prefabs/RealEstateMarket"));
+            this.gameObject.AddComponent<TileDescriptionMenu> ();
+            Destroy(this);
+        }
+    }
+}
