4843f30e3e81e98f49959b5e7b9536ac596a88f0
Varvenne Joan <joan.varvenne@gmail.com>
2014-01-17 13:22:05 +0100

    Event fired when we click on a tile from the REM

--- Assets/Scripts/Events/HandlerClickOnTileFromREM.cs
+++ Assets/Scripts/Events/HandlerClickOnTileFromREM.cs
@@ -1,3 +1,9 @@
+// --------------------------------------------------------------- //
+// 
+// Project : Suburbia
+// Authors : Nemikolh, Pierre mourlanne
+// All Wrongs Reserved.
+// --------------------------------------------------------------- //
 using System;
 
 public interface HandlerClickOnTileFromREM
