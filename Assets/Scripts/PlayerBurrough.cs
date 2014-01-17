79938f3f63f5f5bf1c12b166fb4348b7fae518e0
Varvenne Joan <joan.varvenne@gmail.com>
2014-01-17 13:17:07 +0100

    Rework on the tile view.

---
+++ Assets/Scripts/PlayerBurrough.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerBurrough : MonoBehaviour {
+
+    private Player m_player;
+
+	// Use this for initialization
+	void Start () {
+	    foreach (TileInstance tile in m_player.tiles) {
+            TileView.InstantiateWithParent(tile,this.transform);
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Scripts/Views/TileREMView.cs
@@ -0,0 +1,26 @@
+// --------------------------------------------------------------- //
+// 
+// Project : Suburbia
+// Authors : Nemikolh, Pierre mourlanne
+// All Wrongs Reserved.
+// --------------------------------------------------------------- //
+using System;
+
+public class TileREMView : TileView
+{
+    public void OnMouseEnter ()
+    {
+        Suburbia.Bus.fireEvent (new EventShowTileInformation (true, this.m_tile.description));
+    }
+    
+    public void OnMouseExit ()
+    {
+        Suburbia.Bus.fireEvent (new EventShowTileInformation (false, this.m_tile.description));
+    }
+    
+    public void OnMouseDown ()
+    {
+        Suburbia.Bus.fireEvent (new EventClickedOnTileFromRealEstateMarket (this.m_tile));
+    }
+}
+
