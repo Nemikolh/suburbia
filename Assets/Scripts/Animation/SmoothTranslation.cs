2d55c20efef9263e36d1acc554900f30ab149f01
Varvenne Joan <joan.varvenne@gmail.com>
2014-01-20 17:22:53 +0100

    The transition wasn't started.

--- Assets/Scripts/Animation/SmoothTranslation.cs
+++ Assets/Scripts/Animation/SmoothTranslation.cs
@@ -10,7 +10,7 @@ using UnityEngine;
 public class SmoothTranslation : MonoBehaviour
 {
     private static double LAMBDA = 1.5;
-    private static double FACTOR = (1.0 - Math.Exp( - 5 * LAMBDA * LAMBDA )) / (1.0 - Math.Exp( - 0.25 * LAMBDA * LAMBDA));
+    private static double FACTOR = (1.0 - Math.Exp (- 5 * LAMBDA * LAMBDA)) / (1.0 - Math.Exp (- 0.25 * LAMBDA * LAMBDA));
     private Vector3 m_destination;
     private Vector3 m_origin;
     protected bool m_is_transition_started;
@@ -24,6 +24,11 @@ public class SmoothTranslation : MonoBehaviour
         m_is_transition_started = false;
     }
 
+    public void StartTransition ()
+    {
+        m_is_transition_started = true;
+    }
+
     protected void Update ()
     {
         if (m_is_transition_started) {
@@ -35,10 +40,10 @@ public class SmoothTranslation : MonoBehaviour
         if (m_t < 0) {
             m_t = 0;
         } else if (m_t > LAMBDA * 0.25) {
-            m_t = (float) (LAMBDA * 0.25);
+            m_t = (float)(LAMBDA * 0.25);
             transform.position = m_destination;
         } else {
-            Vector3 delta  = (m_destination - m_origin) * (float) FACTOR;
+            Vector3 delta = (m_destination - m_origin) * (float)FACTOR;
             this.transform.position = ((float)(1.0 - Math.Exp (- m_t * LAMBDA))) * delta + m_origin;
 
         }
