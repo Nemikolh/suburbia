4749b59c57f89a2b6d459b1b3edb5c47b11667a6
Varvenne Joan <joan.varvenne@gmail.com>
2014-01-23 10:20:49 +0100

    Camera controller

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : SmoothTranslation
+{
+
+    // Use this for initialization
+    void Start ()
+    {
+    }
+    
+    // Update is called once per frame
+    protected void Update ()
+    {
+        base.Update();
+    }
+
+    void OnGUI ()
+    {
+        Event ev = Event.current;
+        if (ev.isKey) {
+
+            switch (ev.keyCode) {
+            // We move to the upper player
+            case KeyCode.Z:
+                this.InitWith(new Vector3(0,2.3f,0));
+                break;
+            // We move to the down player
+            case KeyCode.S:
+                this.InitWith(new Vector3());
+                break;
+            // We move to the left player
+            case KeyCode.Q:
+                break;
+            // We move to the right player
+            case KeyCode.D:
+                break;
+            default:
+                break;
+            }
+
+        }
+    }
+}
